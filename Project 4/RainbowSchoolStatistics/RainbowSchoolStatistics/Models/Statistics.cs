c1683b051607819b559410006ea0ead5370855fb
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-09-07 18:50:21 +0530

    Initial commit

---
+++ Project 4/RainbowSchoolStatistics/RainbowSchoolStatistics/Models/Statistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RainbowSchoolStatistics.Models
+{
+    public class Statistics
+    {
+        public int StudentCount { get; set; }
+        public Student MaximumMarks { get; set; }
+        public Student MinimumMarks { get; set; }
+        public List<Student> LessThan65Marks { get; set; }
+        public double PassedPercentage { get; set; }
+
+    }
+}
---
+++ Project 4/RainbowSchoolStatistics/RainbowSchoolStatistics/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RainbowSchoolStatistics.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string StudentName { get; set; }
+        public int Marks { get; set; }
+    }
+}
